9daf574599c56ebdc000d1361724147f0311bfdd
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-03-25 14:36:42 -0600

    moved reducer to its own file

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,68 +1,10 @@
 import { useEffect, useReducer } from "react";
 import axios from "axios";
-
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return {
-        ...state,
-        day: action.day,
-      };
-    case SET_APPLICATION_DATA:
-      return {
-        ...state,
-        days: action.days,
-        appointments: action.appointments,
-        interviewers: action.interviewers,
-      };
-    case SET_INTERVIEW: {
-      const appointment = {
-        ...state.appointments[action.id],
-        interview: action.interview ? { ...action.interview } : null,
-      };
-
-      const appointments = {
-        ...state.appointments,
-        [action.id]: appointment,
-      };
-
-      const days = spotsRemaining(state.day, state.days, appointments);
-
-      return { ...state, appointments, days };
-    }
-     
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
-
-function spotsRemaining(day, days, appointments) {
-  const dayObj = days.find((item) => item.name === day);
-
-  let count = dayObj.appointments.reduce((acc, cur) => {
-    if(!appointments[cur].interview){
-      return ++acc;
-    } else {
-      return acc;
-    }
-  },0);
-
-  const newDaysArray = days.map((item) => {
-    if (item.name === day) {
-      return { ...item, spots: count };
-    }
-    return item;
-  });
-
-  return newDaysArray;
-}
-
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+} from "reducer/reducer";
 
 export default function useApplicationData() {
   const [state, dispatch] = useReducer(reducer, {
---
+++ src/reducer/reducer.js
@@ -0,0 +1,61 @@
+export const SET_DAY = "SET_DAY";
+export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+export const SET_INTERVIEW = "SET_INTERVIEW";
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case SET_DAY:
+      return {
+        ...state,
+        day: action.day,
+      };
+    case SET_APPLICATION_DATA:
+      return {
+        ...state,
+        days: action.days,
+        appointments: action.appointments,
+        interviewers: action.interviewers,
+      };
+    case SET_INTERVIEW: {
+      const appointment = {
+        ...state.appointments[action.id],
+        interview: action.interview ? { ...action.interview } : null,
+      };
+
+      const appointments = {
+        ...state.appointments,
+        [action.id]: appointment,
+      };
+
+      const days = spotsRemaining(state.day, state.days, appointments);
+
+      return { ...state, appointments, days };
+    }
+     
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+function spotsRemaining(day, days, appointments) {
+  const dayObj = days.find((item) => item.name === day);
+
+  let count = dayObj.appointments.reduce((acc, cur) => {
+    if(!appointments[cur].interview){
+      return ++acc;
+    } else {
+      return acc;
+    }
+  },0);
+
+  const newDaysArray = days.map((item) => {
+    if (item.name === day) {
+      return { ...item, spots: count };
+    }
+    return item;
+  });
+
+  return newDaysArray;
+}
